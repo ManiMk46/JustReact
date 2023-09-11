1c535a1a856f00b65f9edc5de087e484cd08acc7
ManiMk46 <mwnimk136@gmail.com>
2023-09-11 13:16:14 +0300

    red color

--- Session3/t3.ts
+++ Session3/t3.ts
@@ -3,7 +3,7 @@ let currentcolor:string = prompt("enter color:").toLowerCase()
 if (currentcolor=== "red"){
     console.log("the next color is green")
 }else if(currentcolor=== "yellow"){
-    console.log("the next color is res")
+    console.log("the next color is red")
 } else if (currentcolor === "green"){
     console.log("the next color is yellow")
 }
