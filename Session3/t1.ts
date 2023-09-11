05b229b88512b3d18ca73373e1be9e200f91f16d
ManiMk46 <mwnimk136@gmail.com>
2023-09-11 13:12:27 +0300

    3ta tamrin aval

---
+++ Session3/t1.ts
@@ -0,0 +1,9 @@
+const userage:number =16;
+const requiredage:number =20;
+
+if (userage < requiredage ){
+    console.log("mojaz nist");
+}else{
+    console.log("welcome")
+}
+
---
+++ Session3/t3.ts
@@ -0,0 +1,11 @@
+let currentcolor:string = prompt("enter color:").toLowerCase()
+
+if (currentcolor=== "red"){
+    console.log("the next color is green")
+}else if(currentcolor=== "yellow"){
+    console.log("the next color is res")
+} else if (currentcolor === "green"){
+    console.log("the next color is yellow")
+}
+
+
