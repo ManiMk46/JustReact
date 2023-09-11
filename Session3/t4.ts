7af2ecdc91fbe3c382f86d0a830ff1c65736aa3f
ManiMk46 <mwnimk136@gmail.com>
2023-09-11 14:35:11 +0300

    tamrin 4om

---
+++ Session3/t4.ts
@@ -0,0 +1,20 @@
+let primeNumbers:number = [1,2,3,4,5,6,7];
+primeNumbers = 8;
+
+if (primeNumbers=== 1){
+    console.log("emrouz shanbas")
+}else if (primeNumbers===2){
+    console.log("emrouz 1shanbas")
+}else if (primeNumbers===3){
+    console.log("emrouz 2shanbas")
+}else if (primeNumbers===4){
+    console.log("emrouz 3shanbas")
+}else if (primeNumbers===5){
+    console.log("emrouz 4shanbas")
+}else if (primeNumbers===6){
+    console.log("emrouz 5shanbas")
+}else if (primeNumbers===7){
+    console.log("emrouz jomast")
+} else {
+    console.log("eshtebas")
+}
